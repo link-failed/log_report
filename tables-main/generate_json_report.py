2ebec271499c6dc06fe9fdf2d12021862447c788
link-failed <huang.ceci@foxmail.com>
2022-08-02 00:10:19 -0700

    update json report

--- tables-main/generate_json_report.py
+++ tables-main/generate_json_report.py
@@ -13,8 +13,11 @@ finish_time_index = 8
 
 
 def generate_or_update_json():
-    log_dict = {}
-    with open("../res/res2.csv", mode='r') as f:
+    # log_dict = {}
+    res_f = open("log_report2.json", "r+")
+    json_dict = json.load(res_f)
+    # json_file.write(log_json)
+    with open("../res/res_test.csv", mode='r') as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
@@ -22,30 +25,43 @@ def generate_or_update_json():
                 execution_time = float(row[execution_time_index])
             else:
                 execution_time = 0
+
             if execution_time > 0:
                 new_id = row[node_name_index] + '[' + row[level_index] + ']'
-                # log_dict.append({
-                #     new_id: {row[start_time_index]: row[execution_time_index]}
-                # })
-                log_dict[new_id] = {row[start_time_index]: row[execution_time_index]}
-    log_json = json.dumps(log_dict, sort_keys=True)
+                start_time = row[start_time_index]
+                duration = row[execution_time_index]
 
-    json_file = open("log_report2.json", "w")
-    json_file.write(log_json)
-    json_file.close()
+                if new_id in json_dict.keys():
+                    json_dict[new_id][start_time] = duration
+                else:
+                    json_dict[new_id] = {start_time: duration}
+
+                res_f.seek(0)
+                res_f.write(json.dumps(json_dict))
+                res_f.truncate()
+    # log_json = json.dumps(log_dict, sort_keys=True)
+    res_f.close()
 
 
 def json_test():
     with open('log_report2.json', mode='r+') as f:
-        json_object = json.load(f)
-        json_object["vitals_first_day[debug]"]["new_time"] = "new_duration"
+        json_dict = json.load(f)
+        json_dict["vitals_first_day[debug]"]["new_time"] = "new_duration"
         f.seek(0)
-        f.write(json.dumps(json_object))
+        f.write(json.dumps(json_dict))
         f.truncate()
 
 
-# generate_or_update_json()
-json_test()
+def json_test2():
+    with open('log_report2.json', mode='r+') as f:
+        json_dict = json.load(f)
+        print(type(json_dict))
+        if "rrt_first_day[debug]" in json_dict.keys():
+            print("in")
+        else:
+            print("not in")
 
 
-
+generate_or_update_json()
+# json_test()
+# json_test2()
