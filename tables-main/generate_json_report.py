47d78e0646dfef13b7142f030fa88daac9587d8b
link-failed <huang.ceci@foxmail.com>
2022-08-01 23:39:42 -0700

    generate json

--- tables-main/generate_json_report.py
+++ tables-main/generate_json_report.py
@@ -9,11 +9,11 @@ finish_time_index = 8
 
 
 # json file format:
-# {'model_name': {'start_time1': 'duration', 'start_time2': 'duration', ...}, 'level': 'level'}
+# {'model_name-level': {'start_time1': 'duration', 'start_time2': 'duration', ...}}
 
 
 def generate_or_update_json():
-    log_dict = []
+    log_dict = {}
     with open("../res/res2.csv", mode='r') as f:
         reader = csv.reader(f)
         next(reader)
@@ -23,17 +23,29 @@ def generate_or_update_json():
             else:
                 execution_time = 0
             if execution_time > 0:
-                log_dict.append({
-                    row[node_name_index]: {row[start_time_index]: row[execution_time_index]},
-                    "level": row[level_index]
-                })
-        return json.dumps(log_dict, sort_keys=True)
+                new_id = row[node_name_index] + '[' + row[level_index] + ']'
+                # log_dict.append({
+                #     new_id: {row[start_time_index]: row[execution_time_index]}
+                # })
+                log_dict[new_id] = {row[start_time_index]: row[execution_time_index]}
+    log_json = json.dumps(log_dict, sort_keys=True)
+
+    json_file = open("log_report2.json", "w")
+    json_file.write(log_json)
+    json_file.close()
 
 
 def json_test():
+    with open('log_report2.json', mode='r+') as f:
+        json_object = json.load(f)
+        json_object["vitals_first_day[debug]"]["new_time"] = "new_duration"
+        f.seek(0)
+        f.write(json.dumps(json_object))
+        f.truncate()
 
 
-jsonFile = open("log_report.json", "w")
-jsonFile.write(log_info_for_table())
-jsonFile.close()
+# generate_or_update_json()
+json_test()
+
+
 
